ccbb4d1b0e327cf1895f85ec444a9fd649d80ed2
Arli <arli.machado@gmail.com>
2021-08-06 17:08:15 -0300

    configurando prettier, eslint

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 
-import {
-  Container,
-  Title
-} from './styles';
+import { Container, Title } from './styles';
 
-export function Home(){
-
+export function Home() {
   return (
     <Container>
-      <Title>
-        Home
-      </Title>
+      <Title>Home</Title>
     </Container>
   );
-};
+}
