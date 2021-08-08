1a0f5199a1e34361e078a88d3d08789f8cdef80c
Arli <arli.machado@gmail.com>
2021-08-07 21:42:01 -0300

    Definido componente Card car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,10 +3,33 @@ import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import { Container, Header, HeaderContent, TotalCars } from './styles';
 
 export function Home() {
+  const carOne = {
+    brand: 'audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'ao dia',
+      price: 120,
+    },
+    thumbnail:
+      'https://www.motortrend.com/uploads/sites/10/2018/05/2018-audi-rs5-4wd-coupe-angular-front.png',
+  };
+
+  const carTwo = {
+    brand: 'porche',
+    name: 'Panamera',
+    rent: {
+      period: 'ao dia',
+      price: 120,
+    },
+    thumbnail:
+      'https://catalogo.webmotors.com.br/imagens/prod/347468/PORSCHE_PANAMERA_2.9_V6_EHYBRID_4_PDK_3474681900348621.png?s=fill&w=236&h=135&q=70&t=true',
+  };
+
   return (
     <Container>
       <StatusBar
@@ -21,6 +44,9 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carOne} />
+      <Car data={carTwo} />
     </Container>
   );
 }
